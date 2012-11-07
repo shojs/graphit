36dcad9826a6f160fc6b9c661fa039bf456be484
sho <showi.nos666@gmail.com>
2012-11-07 01:23:09 +0100

    Removing debug message

--- js/app/Cdraw_grapher.js
+++ js/app/Cdraw_grapher.js
@@ -31,52 +31,35 @@ function math_linear_interpolation(p1, p2, step) {
 	var points = new Array();
 	var x1 = p1.x;
 	var x2 = p2.x;
-	if (p1.x >= p2.x) { x1 = p2.x; x2 = p1.x}
+	if (p1.x >= p2.x) { x1 = p2.x; x2 = p1.x; }
 	var slope = (p2.y - p1.y) / (p2.x - p1.x);
 	for (var x = x1; x <= x2; x += step) {
 		var y = Math.round(slope * (x - p1.x) + p1.y);
-		//console.log('x: ' + x + ', y: ' + y);
 		points.push(new Cpoint(x, y));
 	}
 	return points;
 };
 
 Cdraw_grapher.prototype._graph = function() {
-	//console.log('Graphing');
 	var numpoint = cSurface.mouse.points.length;
 	if (numpoint <= 2 ) {
-		//helper_draw_circle(e, p1.x, p1.y, radius, color);
-		//this.index++;
-		console.log('need two points');
+		//console.log('need two points');
 		return false;
 	}
 	if (this.index >= (numpoint - 1)) {
-		console.log('nothing to graph');
+		//console.log('nothing to graph');
 		return false;
 	}
 	var e = document.getElementById('graphing-area');
-	//var ctx = e.getContext('2d');
 	var radius = this.cTools.size / 2;
 	var color = this.cTools.color.to_rgba();
 	
 	var p1 = this.cSurface.mouse.points[this.index];
 	var p2 = this.cSurface.mouse.points[(this.index + 1)];
-	console.log('points: ', p1, p2);
-	//var distance = geom_distance(p1, p2);
-//	console.log('P1');
-//	console.log(p1);
-//	console.log('P2');
-//	console.log(p2);
-//	if (distance < radius) {
-//		helper_draw_circle(e, p1.x, p1.y, radius, color);
-//	} else {
 		var points = math_linear_interpolation(p1, p2, radius);
 		for (var i = 0; i < points.length; i++) {
-			//console.log('interpolate: ', points[i].x,  points[i].y);
 			helper_draw_circle(e, points[i].x, points[i].y, radius, color);
 		}
-//	}
-	//console.log('Distance: ' + geom_distance(p0, p));
 	this.index++;
 	this._graph();
 	return true;
