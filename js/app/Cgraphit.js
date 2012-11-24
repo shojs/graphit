bf4bae0513140421261157a0113e221b5a55c4f4
Sho <joachim.basmaison@gmail.com>
2012-11-24 14:48:02 +0100

    Doctype Html5

--- js/app/Cgraphit.js
+++ js/app/Cgraphit.js
@@ -256,6 +256,11 @@ Cgraphit.prototype.dom_build = function() {
     '<img style="border:0;width:44px;height:16px"' +
     '    src="http://jigsaw.w3.org/css-validator/images/vcss"' +
     '    alt="Valid CSS!" \/></a></p>');
+	r.append('<p>' +
+		     '<a href="http://jigsaw.w3.org/css-validator/check/referer">' +
+	         '<img style="border:0;width:88px;height:31px"' +
+	         '  src="http://jigsaw.w3.org/css-validator/images/vcss"' + 
+	         '   alt="Valid CSS!" /></a></p>');
 	this.rootElm = r;
 	return this;
 };
